5be920676840cd2bbd97f6e54cb932afb77c1b48
Juan Cazarez <jm.cazarez.sepulveda@gmail.com>
2025-10-14 18:42:31 -0400

    added dummie logic for Staff and Student access.

--- front_end/src/pages/Login.jsx
+++ front_end/src/pages/Login.jsx
@@ -1,8 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
     <div style={{ textAlign: "center", marginTop: "3rem" }}>
       <h1>Login Page</h1>
-      <p>This page allows users to log in to their accounts.</p>
+      <p>Select a role to continue:</p>
+
+      <Link to="/student-dashboard" style={btnStyle}>
+        Continue as Student
+      </Link>
+
+      <br /><br />
+
+      <Link to="/staff-dashboard" style={btnStyle}>
+        Continue as Staff
+      </Link>
     </div>
   );
 }
+
+const btnStyle = {
+  color: "white",
+  background: "blue",
+  padding: "10px 15px",
+  borderRadius: "5px",
+  textDecoration: "none",
+};
