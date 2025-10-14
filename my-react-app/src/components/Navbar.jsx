46048797ac1b72a703695c4b09c9583e506976b7
MGuereca1 <mguerecarochell1@student.gsu.edu>
2025-10-14 19:44:41 -0400

    first commit

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import {X, Menu} from 'lucide-react'  //npm install lucide-react
+import placeholder from '../assets/placeholder.png'
+import {navItems } from "../content/index"
+
+const Navbar = () => {
+    // state hook for mobile and for sign/out
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false)
+    const [isLoggedIn, SetisLoggedIn] = useState(false)
+
+    const toggleNavbar = () =>{
+        setMobileDrawerOpen(!mobileDrawerOpen)
+    }
+
+    const handleLogin = () => {
+        SetisLoggedIn(!isLoggedIn)
+    }
+
+  return (
+    <div className="navbar">
+        {/* logo*/}
+        <img src={placeholder} alt="logo" className="logo"/>
+
+        {/* desktop nav */}
+        <ul className="dekstop-nav">
+            {navItems.map((item, index) => (
+                <li key={index}>
+                    <a href={item.href}>{item.label}</a>
+                </li>
+            ))}
+        </ul>
+
+        {/* sign in button */}
+        <button onClick={handleLogin} className="signin-bttn">
+            {isLoggedIn ? "Sign Out" : "SignIn"}
+        </button>
+
+        {/* mobile menu */}
+        <div className="Mobile-menu-toggle">
+            <button onClick={toggleNavbar} className="menu-bttn">
+                {mobileDrawerOpen ? <X/> : <Menu/>}    
+            </button>
+        </div>
+        {/* will show drawer only on mobile device, fix css */}
+        {mobileDrawerOpen && (
+            <div className="mobile-nav">
+                <ul>
+                    {navItems.map((item, index) => (
+                    <li key={index}>
+                        <a href={item.href}>{item.label}</a>
+                    </li>
+                ))}
+                </ul>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Navbar
---
+++ my-react-app/src/content/index.jsx
@@ -0,0 +1,11 @@
+// any content we have on the site can be placed here
+// for example navItems, about info, any links
+
+// navItems used for navbar
+export const navItems = [
+    { label: 'Home', href:''},
+    { label: 'Appointments', href:''},
+    { label: 'Prescriptions', href:''},
+    { label: 'Records', href:''},
+    { label: 'Settings', href:''},
+]
